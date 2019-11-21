eee91da4de247cb234fe9bbf05ca194773b9b292
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-11-21 20:15:00 +0100

    update to intro game start art

--- gameFolder/gameIntro.h
+++ gameFolder/gameIntro.h
@@ -5,19 +5,58 @@ void startGame(string name);
 int playerAttribute(int &strength, int &health, int &luck, int &totalPoints);
 int reset(int &strength, int &health, int &luck, int &totalPoints);
 
+void clear()
+{
+    cout << string(20, '\n');
+}
 void gameIntro()
 {
     string playerReady{};
-    cout << "=====================Welcome======================"
-         << "\n Are you ready to begin your adventure ( press Y to start ) \n";
+    cout << R"(
+           /\                                                        /\
+          |  |                                                      |  |
+         /----\                                                    /----\
+        [______]             Welcome to YOUR adventure            [______]
+         |    |         _____                        _____         |    |
+         |[]  |        [     ]                      [     ]        |  []|
+         |    |       [_______][ ][ ][ ][][ ][ ][ ][_______]       |    |
+         |    [ ][ ][ ]|     |  ,----------------,  |     |[ ][ ][ ]    |
+         |             |     |/'    ____..____    '\|     |             |
+          \  []        |     |    /'    ||    '\    |     |        []  /
+           |      []   |     |   |o     ||     o|   |     |  []       |
+           |           |  _  |   |     _||_     |   |  _  |           |
+           |   []      | (_) |   |    (_||_)    |   | (_) |       []  |
+           |           |     |   |     (||)     |   |     |           |
+           |           |     |   |      ||      |   |     |           |
+         /''           |     |   |o     ||     o|   |     |           ''\
+        [_____________[_______]--'------''------'--[_______]_____________])"
+         << endl;
+    cout << "\n      >>------>    to begin your adventure ( press Y to start )    >>------>  \n";
     cin >> playerReady;
-
+    clear();
     if (playerReady == "y")
     {
 
         string name{};
 
-        cout << "\n Great - Please enter your Player name... test.. ";
+        cout << "\n"
+             << R"(
+                          o
+                            /\
+                           /::\
+                          /::::\
+            ,a_a         /\::::/\
+           {/ ''\_      /\ \::/\ \
+           {\ ,_oo)    /\ \ \/\ \ \
+           {/  (_^____/  \ \ \ \ \ \           Please ENTER YOUR NAME .....
+ .=.      {/ \___)))*)    \ \ \ \ \/
+(.=.`\   {/   /=;  ~/      \ \ \ \/
+    \ `\{/(   \/\  /        \ \ \/
+     \  `. `\  ) )           \ \/
+      \    // /_/_            \/
+       '==''---))))
+        )" << endl;
+
         cin >> name;
         startGame(name);
     }
@@ -29,19 +68,38 @@ void gameIntro()
 
 void startGame(string name)
 {
+    clear();
     // set up of attributes
     int strength{};
     int health{};
     int luck{};
     int totalPoints{5};
     string confirm{};
-    cout << "\n Welcome " << name << "\n ========== Please build your character ============= \n";
-    cout << "you have FIVE (5) points to spend in THREE key areas : ";
-    cout << "\n -Strength \n -Health \n -Luck \n Please select carefully, as your choices will effect gameplay \n";
+
+    cout << "\n"
+         << R"(
+   __________________________________________________________________________________
+ / \                                     Welcome                                      \.
+|   |                ========== Please build your character =============              |.
+ \_ |                you have FIVE (5) points to spend in THREE key areas :            |.
+    |                                                                                  |.
+    |                                   -Strength                                      |.
+    |                                                                                  |.
+    |                                   -Health                                        |.
+    |                                                                                  |.
+    |                                   -Luck                                          |.
+    |                                                                                  |.
+    |               Please select carefully, as your choices will effect gameplay      |.
+    |                                                                                  |.
+    |                                                                                  |.
+    |                                                                                  |.
+    |   _______________________________________________________________________________|
+    |  /                                                                              /.
+    \_/______________________________________________________________________________/.
+)" << endl;
 
     playerAttribute(strength, health, luck, totalPoints);
-    // if loop for remaining points if left over
-    // need WHILE LOOP not IF
+
     while (totalPoints > 0 || totalPoints < 0)
     {
         reset(strength, health, luck, totalPoints);
@@ -54,7 +112,6 @@ void startGame(string name)
 
     if (confirm == "y")
     {
-
         // GAME START ==================================
     }
     else
@@ -63,21 +120,17 @@ void startGame(string name)
         reset(strength, health, luck, totalPoints);
         playerAttribute(strength, health, luck, totalPoints);
     }
-
-    // if loop to re enter if incorrect
 }
 
 int reset(int &strength, int &health, int &luck, int &totalPoints)
 {
 
-    cout << "\n please try again and pay attention \n"<< endl;
+    cout << "\n please try again and pay attention \n"
+         << endl;
     strength = 0;
     health = 0;
     luck = 0;
     totalPoints = 5;
 
-      return strength,health,luck,totalPoints;
+    return strength, health, luck, totalPoints;
 }
-// TO DO
-
-// ADD LOOPS FOR VALIDATION AND RESET
--- gameFolder/myBag.h
+++ gameFolder/myBag.h
@@ -7,16 +7,25 @@ void myBag();
 
 void myBag () {
 
-cout << " \n \n \n =========================== GAME BAGGGGGGGGGGGGGG ===========================";
+cout << " \n \n \n =========================== GAME BAGGGGGGGGGGGGGG ===========================" << endl;
 
 
-vector <string> iventory{
+vector <string> inventory{
     "shield",
     "sword",
     "coat"
 };
 
 
+cout << "\n your current bag has : \n" << endl;
 
+
+// BAG INVENTORY HERE ASCII ART
+
+
+for ( int i =0; i < inventory.size(); i++) {
+
+cout << "               "<< inventory[i] << "             " << "\n" << endl;
+}
 
 }
